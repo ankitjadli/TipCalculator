55442f3142ebd3ef5bf9580fad220dcd139732c0
ankit <ankitjadli26@gmail.com>
2021-05-28 23:01:01 +0530

    Added Buttons

--- TipCalculator/Form1.Designer.cs
+++ TipCalculator/Form1.Designer.cs
@@ -39,22 +39,33 @@ namespace TipCalculator
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pplplus = new System.Windows.Forms.Button();
+            this.pplminus = new System.Windows.Forms.Button();
+            this.tipplus = new System.Windows.Forms.Button();
+            this.tipminus = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(90, 198);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(215, 159);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calculate";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 76);
+            this.label2.Location = new System.Drawing.Point(121, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 13);
             this.label2.TabIndex = 2;
@@ -62,6 +73,7 @@ namespace TipCalculator
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.textBox1.Location = new System.Drawing.Point(124, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
@@ -69,33 +81,35 @@ namespace TipCalculator
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 73);
+            this.textBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox2.Location = new System.Drawing.Point(141, 83);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(83, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 155);
+            this.label3.Location = new System.Drawing.Point(308, 36);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.Size = new System.Drawing.Size(77, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Price Per Person";
+            this.label3.Text = "Tip Per Person";
             // 
             // labeltax
             // 
-            this.labeltax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labeltax.Location = new System.Drawing.Point(124, 150);
+            this.labeltax.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labeltax.Font = new System.Drawing.Font("Leelawadee", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltax.Location = new System.Drawing.Point(391, 27);
             this.labeltax.Name = "labeltax";
-            this.labeltax.Size = new System.Drawing.Size(100, 23);
+            this.labeltax.Size = new System.Drawing.Size(128, 36);
             this.labeltax.TabIndex = 6;
-            this.labeltax.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labeltax.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(230, 76);
+            this.label4.Location = new System.Drawing.Point(138, 67);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(15, 13);
             this.label4.TabIndex = 7;
@@ -103,34 +117,111 @@ namespace TipCalculator
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(124, 109);
+            this.textBox3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox3.Location = new System.Drawing.Point(141, 128);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(83, 20);
             this.textBox3.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(46, 112);
+            this.label5.Location = new System.Drawing.Point(121, 112);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.Size = new System.Drawing.Size(91, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "Split Between";
+            this.label5.Text = "Number of people";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 39);
+            this.label1.Location = new System.Drawing.Point(123, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(20, 13);
             this.label1.TabIndex = 13;
-            this.label1.Text = "Bill Amount";
+            this.label1.Text = "Bill";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(138, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(308, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Total Per Person";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Font = new System.Drawing.Font("Leelawadee", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(400, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(175, 70);
+            this.label8.TabIndex = 16;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pplplus
+            // 
+            this.pplplus.Location = new System.Drawing.Point(215, 126);
+            this.pplplus.Name = "pplplus";
+            this.pplplus.Size = new System.Drawing.Size(27, 23);
+            this.pplplus.TabIndex = 17;
+            this.pplplus.Text = "+";
+            this.pplplus.UseVisualStyleBackColor = true;
+            this.pplplus.Click += new System.EventHandler(this.pplplus_Click);
+            // 
+            // pplminus
+            // 
+            this.pplminus.Location = new System.Drawing.Point(120, 126);
+            this.pplminus.Name = "pplminus";
+            this.pplminus.Size = new System.Drawing.Size(23, 23);
+            this.pplminus.TabIndex = 18;
+            this.pplminus.Text = "-";
+            this.pplminus.UseVisualStyleBackColor = true;
+            this.pplminus.Click += new System.EventHandler(this.pplminus_Click);
+            // 
+            // tipplus
+            // 
+            this.tipplus.Location = new System.Drawing.Point(215, 81);
+            this.tipplus.Name = "tipplus";
+            this.tipplus.Size = new System.Drawing.Size(22, 23);
+            this.tipplus.TabIndex = 19;
+            this.tipplus.Text = "+";
+            this.tipplus.UseVisualStyleBackColor = true;
+            this.tipplus.Click += new System.EventHandler(this.tipplus_Click);
+            // 
+            // tipminus
+            // 
+            this.tipminus.Location = new System.Drawing.Point(121, 81);
+            this.tipminus.Name = "tipminus";
+            this.tipminus.Size = new System.Drawing.Size(22, 23);
+            this.tipminus.TabIndex = 20;
+            this.tipminus.Text = "-";
+            this.tipminus.UseVisualStyleBackColor = true;
+            this.tipminus.Click += new System.EventHandler(this.tipminus_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 319);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(576, 208);
+            this.Controls.Add(this.tipminus);
+            this.Controls.Add(this.tipplus);
+            this.Controls.Add(this.pplminus);
+            this.Controls.Add(this.pplplus);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox3);
@@ -160,6 +251,13 @@ namespace TipCalculator
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button pplplus;
+        private System.Windows.Forms.Button pplminus;
+        private System.Windows.Forms.Button tipplus;
+        private System.Windows.Forms.Button tipminus;
     }
 }
 
--- TipCalculator/Form1.cs
+++ TipCalculator/Form1.cs
@@ -19,15 +19,66 @@ namespace TipCalculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double bill, tip, total,noofpoeple;
-            bill = double.Parse(textBox1.Text);
-            noofpoeple = double.Parse(textBox3.Text);
-            tip = double.Parse(textBox2.Text);
-            total = bill + (tip / 100) * bill;
-            total = total / noofpoeple;
-            labeltax.Text = total.ToString();
-
+            if (String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox3.Text))
+            {
+                label6.Text = ("Field must not be empty");
+            }
+            else
+            {
+                double bill, tip, total, noofpoeple,perperson;
+                bill = double.Parse(textBox1.Text);
+                noofpoeple = double.Parse(textBox3.Text);
+                tip = double.Parse(textBox2.Text);
+                if (bill < 0 || noofpoeple < 0 || tip < 0)
+                {
+                    label6.Text = ("Negetive Text Field");
+                }
+                else
+                {
+                    label6.Text = ("");
+                    total = (tip / 100) * bill;
+                    total = Math.Round(total / noofpoeple);
+                    perperson = Math.Round(total + (bill / noofpoeple));
+                    labeltax.Text = ("$") +  total.ToString();
+                    label8.Text = ("$") + perperson.ToString();
+                }
+            }
         }
 
+        private void tipminus_Click(object sender, EventArgs e)
+        {
+            if(double.Parse(textBox2.Text)>0)
+            {
+                double temp = double.Parse(textBox2.Text) - 1;
+                textBox2.Text = temp.ToString();
+            }
+        }
+
+        private void tipplus_Click(object sender, EventArgs e)
+        {
+            if (double.Parse(textBox2.Text)<100)
+            {
+                double temp = double.Parse(textBox2.Text) + 1;
+                textBox2.Text = temp.ToString();
+            }
+        }
+
+        private void pplplus_Click(object sender, EventArgs e)
+        {
+            if (double.Parse(textBox3.Text) < 100)
+            {
+                double temp = double.Parse(textBox3.Text) + 1;
+                textBox3.Text = temp.ToString();
+            }
+        }
+
+        private void pplminus_Click(object sender, EventArgs e)
+        {
+            if (double.Parse(textBox3.Text)>0)
+            {
+                double temp = double.Parse(textBox3.Text) - 1;
+                textBox3.Text = temp.ToString();
+            }
+        }
     }
 }
