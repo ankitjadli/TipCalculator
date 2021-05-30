ceda19ae7814216ed6ca96ebcb6bde639bbac2a6
ankit <ankitjadli26@gmail.com>
2021-05-30 06:46:30 +0530

    Added Comments and Label Names.

--- TipCalculator/Form1.Designer.cs
+++ TipCalculator/Form1.Designer.cs
@@ -30,16 +30,16 @@ namespace TipCalculator
         private void InitializeComponent()
         {
             this.Tip_Percentage_Label = new System.Windows.Forms.Label();
-            this.Bill_amount_Textbox = new System.Windows.Forms.TextBox();
+            this.Bill_Amount_Textbox = new System.Windows.Forms.TextBox();
             this.Tip_Percentage_TextBox = new System.Windows.Forms.TextBox();
             this.Tip_Per_Person_Label = new System.Windows.Forms.Label();
-            this.labeltax = new System.Windows.Forms.Label();
+            this.Tip_Per_Person_Result = new System.Windows.Forms.Label();
             this.Number_Of_People_TextBox = new System.Windows.Forms.TextBox();
             this.Number_Of_People_Label = new System.Windows.Forms.Label();
             this.Bill_Amount_Label = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.Empty_Field_Alert_Label = new System.Windows.Forms.Label();
             this.Total_Per_Person_Label = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.Total_Per_Person_Result = new System.Windows.Forms.Label();
             this.Number_Of_People_Increase_Button = new System.Windows.Forms.Button();
             this.Number_Of_People_Decrease_Button = new System.Windows.Forms.Button();
             this.Tip_Increase_Button = new System.Windows.Forms.Button();
@@ -55,17 +55,17 @@ namespace TipCalculator
             this.Tip_Percentage_Label.TabIndex = 2;
             this.Tip_Percentage_Label.Text = "Tip %";
             // 
-            // Bill_amount_Textbox
+            // Bill_Amount_Textbox
             // 
-            this.Bill_amount_Textbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Bill_amount_Textbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Bill_amount_Textbox.Location = new System.Drawing.Point(21, 41);
-            this.Bill_amount_Textbox.Name = "Bill_amount_Textbox";
-            this.Bill_amount_Textbox.Size = new System.Drawing.Size(122, 20);
-            this.Bill_amount_Textbox.TabIndex = 3;
-            this.Bill_amount_Textbox.Text = "0";
-            this.Bill_amount_Textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            this.Bill_amount_Textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.Bill_Amount_Textbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Bill_Amount_Textbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Bill_Amount_Textbox.Location = new System.Drawing.Point(21, 41);
+            this.Bill_Amount_Textbox.Name = "Bill_Amount_Textbox";
+            this.Bill_Amount_Textbox.Size = new System.Drawing.Size(122, 20);
+            this.Bill_Amount_Textbox.TabIndex = 3;
+            this.Bill_Amount_Textbox.Text = "0";
+            this.Bill_Amount_Textbox.TextChanged += new System.EventHandler(this.Bill_Amount_Textbox_TextChanged);
+            this.Bill_Amount_Textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Bill_Amount_Textbox_KeyPress);
             // 
             // Tip_Percentage_TextBox
             // 
@@ -76,8 +76,8 @@ namespace TipCalculator
             this.Tip_Percentage_TextBox.Size = new System.Drawing.Size(83, 20);
             this.Tip_Percentage_TextBox.TabIndex = 4;
             this.Tip_Percentage_TextBox.Text = "0";
-            this.Tip_Percentage_TextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            this.Tip_Percentage_TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.Tip_Percentage_TextBox.TextChanged += new System.EventHandler(this.Tip_Percentage_TextBox_TextChanged);
+            this.Tip_Percentage_TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tip_Percentage_TextBox_KeyPress);
             // 
             // Tip_Per_Person_Label
             // 
@@ -88,15 +88,15 @@ namespace TipCalculator
             this.Tip_Per_Person_Label.TabIndex = 5;
             this.Tip_Per_Person_Label.Text = "Tip Per Person";
             // 
-            // labeltax
+            // Tip_Per_Person_Result
             // 
-            this.labeltax.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.labeltax.Font = new System.Drawing.Font("Leelawadee", 8.25F);
-            this.labeltax.Location = new System.Drawing.Point(292, 48);
-            this.labeltax.Name = "labeltax";
-            this.labeltax.Size = new System.Drawing.Size(250, 20);
-            this.labeltax.TabIndex = 6;
-            this.labeltax.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Tip_Per_Person_Result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tip_Per_Person_Result.Font = new System.Drawing.Font("Leelawadee", 8.25F);
+            this.Tip_Per_Person_Result.Location = new System.Drawing.Point(292, 48);
+            this.Tip_Per_Person_Result.Name = "Tip_Per_Person_Result";
+            this.Tip_Per_Person_Result.Size = new System.Drawing.Size(250, 20);
+            this.Tip_Per_Person_Result.TabIndex = 6;
+            this.Tip_Per_Person_Result.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Number_Of_People_TextBox
             // 
@@ -106,8 +106,8 @@ namespace TipCalculator
             this.Number_Of_People_TextBox.Size = new System.Drawing.Size(83, 20);
             this.Number_Of_People_TextBox.TabIndex = 9;
             this.Number_Of_People_TextBox.Text = "1";
-            this.Number_Of_People_TextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            this.Number_Of_People_TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            this.Number_Of_People_TextBox.TextChanged += new System.EventHandler(this.Number_Of_People_TextBox_TextChanged);
+            this.Number_Of_People_TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Number_Of_People_TextBox_KeyPress);
             // 
             // Number_Of_People_Label
             // 
@@ -127,14 +127,14 @@ namespace TipCalculator
             this.Bill_Amount_Label.TabIndex = 13;
             this.Bill_Amount_Label.Text = "Bill";
             // 
-            // label6
+            // Empty_Field_Alert_Label
             // 
-            this.label6.AutoSize = true;
-            this.label6.ForeColor = System.Drawing.Color.Red;
-            this.label6.Location = new System.Drawing.Point(241, 163);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 13);
-            this.label6.TabIndex = 14;
+            this.Empty_Field_Alert_Label.AutoSize = true;
+            this.Empty_Field_Alert_Label.ForeColor = System.Drawing.Color.Red;
+            this.Empty_Field_Alert_Label.Location = new System.Drawing.Point(241, 163);
+            this.Empty_Field_Alert_Label.Name = "Empty_Field_Alert_Label";
+            this.Empty_Field_Alert_Label.Size = new System.Drawing.Size(0, 13);
+            this.Empty_Field_Alert_Label.TabIndex = 14;
             // 
             // Total_Per_Person_Label
             // 
@@ -145,15 +145,15 @@ namespace TipCalculator
             this.Total_Per_Person_Label.TabIndex = 15;
             this.Total_Per_Person_Label.Text = "Total Per Person";
             // 
-            // label8
+            // Total_Per_Person_Result
             // 
-            this.label8.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label8.Font = new System.Drawing.Font("Leelawadee", 8.25F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(301, 91);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(241, 21);
-            this.label8.TabIndex = 16;
-            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Total_Per_Person_Result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Total_Per_Person_Result.Font = new System.Drawing.Font("Leelawadee", 8.25F, System.Drawing.FontStyle.Bold);
+            this.Total_Per_Person_Result.Location = new System.Drawing.Point(301, 91);
+            this.Total_Per_Person_Result.Name = "Total_Per_Person_Result";
+            this.Total_Per_Person_Result.Size = new System.Drawing.Size(241, 21);
+            this.Total_Per_Person_Result.TabIndex = 16;
+            this.Total_Per_Person_Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Number_Of_People_Increase_Button
             // 
@@ -163,7 +163,7 @@ namespace TipCalculator
             this.Number_Of_People_Increase_Button.TabIndex = 17;
             this.Number_Of_People_Increase_Button.Text = "+";
             this.Number_Of_People_Increase_Button.UseVisualStyleBackColor = true;
-            this.Number_Of_People_Increase_Button.Click += new System.EventHandler(this.pplplus_Click);
+            this.Number_Of_People_Increase_Button.Click += new System.EventHandler(this.Number_Of_People_Increase_Button_Click);
             // 
             // Number_Of_People_Decrease_Button
             // 
@@ -173,7 +173,7 @@ namespace TipCalculator
             this.Number_Of_People_Decrease_Button.TabIndex = 18;
             this.Number_Of_People_Decrease_Button.Text = "-";
             this.Number_Of_People_Decrease_Button.UseVisualStyleBackColor = true;
-            this.Number_Of_People_Decrease_Button.Click += new System.EventHandler(this.pplminus_Click);
+            this.Number_Of_People_Decrease_Button.Click += new System.EventHandler(this.Number_Of_People_Decrease_Button_Click);
             // 
             // Tip_Increase_Button
             // 
@@ -183,7 +183,7 @@ namespace TipCalculator
             this.Tip_Increase_Button.TabIndex = 19;
             this.Tip_Increase_Button.Text = "+";
             this.Tip_Increase_Button.UseVisualStyleBackColor = true;
-            this.Tip_Increase_Button.Click += new System.EventHandler(this.tipplus_Click);
+            this.Tip_Increase_Button.Click += new System.EventHandler(this.Tip_Increase_Button_Click);
             // 
             // Tip_Decrease_Button
             // 
@@ -193,7 +193,7 @@ namespace TipCalculator
             this.Tip_Decrease_Button.TabIndex = 20;
             this.Tip_Decrease_Button.Text = "-";
             this.Tip_Decrease_Button.UseVisualStyleBackColor = true;
-            this.Tip_Decrease_Button.Click += new System.EventHandler(this.tipminus_Click);
+            this.Tip_Decrease_Button.Click += new System.EventHandler(this.Tip_Decrease_Button_Click);
             // 
             // Form1
             // 
@@ -205,16 +205,16 @@ namespace TipCalculator
             this.Controls.Add(this.Tip_Increase_Button);
             this.Controls.Add(this.Number_Of_People_Decrease_Button);
             this.Controls.Add(this.Number_Of_People_Increase_Button);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Total_Per_Person_Result);
             this.Controls.Add(this.Total_Per_Person_Label);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Empty_Field_Alert_Label);
             this.Controls.Add(this.Bill_Amount_Label);
             this.Controls.Add(this.Number_Of_People_Label);
             this.Controls.Add(this.Number_Of_People_TextBox);
-            this.Controls.Add(this.labeltax);
+            this.Controls.Add(this.Tip_Per_Person_Result);
             this.Controls.Add(this.Tip_Per_Person_Label);
             this.Controls.Add(this.Tip_Percentage_TextBox);
-            this.Controls.Add(this.Bill_amount_Textbox);
+            this.Controls.Add(this.Bill_Amount_Textbox);
             this.Controls.Add(this.Tip_Percentage_Label);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -225,16 +225,16 @@ namespace TipCalculator
 
         #endregion
         private System.Windows.Forms.Label Tip_Percentage_Label;
-        private System.Windows.Forms.TextBox Bill_amount_Textbox;
+        private System.Windows.Forms.TextBox Bill_Amount_Textbox;
         private System.Windows.Forms.TextBox Tip_Percentage_TextBox;
         private System.Windows.Forms.Label Tip_Per_Person_Label;
-        private System.Windows.Forms.Label labeltax;
+        private System.Windows.Forms.Label Tip_Per_Person_Result;
         private System.Windows.Forms.TextBox Number_Of_People_TextBox;
         private System.Windows.Forms.Label Number_Of_People_Label;
         private System.Windows.Forms.Label Bill_Amount_Label;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Empty_Field_Alert_Label;
         private System.Windows.Forms.Label Total_Per_Person_Label;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label Total_Per_Person_Result;
         private System.Windows.Forms.Button Number_Of_People_Increase_Button;
         private System.Windows.Forms.Button Number_Of_People_Decrease_Button;
         private System.Windows.Forms.Button Tip_Increase_Button;
--- TipCalculator/Form1.cs
+++ TipCalculator/Form1.cs
@@ -16,91 +16,107 @@ namespace TipCalculator
         public Form1()
         {
             InitializeComponent();
-
         }
-        private void Calculate_Tip()
+        //This method Takes Values from all 3 TextBoxes and Calculate result.
+        private void Calculate_Tip() 
         {
-            if (String.IsNullOrEmpty(Bill_amount_Textbox.Text) || String.IsNullOrEmpty(Tip_Percentage_TextBox.Text) || String.IsNullOrEmpty(Number_Of_People_TextBox.Text))
+            /*Below if statement checks if any of the 3 TextBoxes don't have any value
+             * if not, Empty_Field_Label shows a text.
+             * else we calculate our result.
+             */
+
+            if (String.IsNullOrEmpty(Bill_Amount_Textbox.Text) || String.IsNullOrEmpty(Tip_Percentage_TextBox.Text) || String.IsNullOrEmpty(Number_Of_People_TextBox.Text))
             {
-                label6.Text = ("Field must not be empty");
+                Empty_Field_Alert_Label.Text = ("Field must not be empty");
             }
             else
             {
-                double Bill_Amount=0, Tip_Percentage=0, Total_Per_Person=0, Number_Of_People=0, Tip_Per_Person=0;
+                double Bill_Amount,Tip_Percentage,Total_Per_Person,Number_Of_People,Tip_Per_Person;
+
+                /*Below try block takes Bill_Amount and check if it exceeds maximum capacity
+                 * of "double" data type.
+                */
+
                 try
                 {
-                    Bill_Amount = double.Parse(Bill_amount_Textbox.Text);
+                    Bill_Amount = double.Parse(Bill_Amount_Textbox.Text);
                 }
                 catch (System.OverflowException)
                 {
                     Bill_Amount = 0;
-                    Bill_amount_Textbox.Text = "0";
+                    Bill_Amount_Textbox.Text = "0"; //resets Bill Amount
                 }
-                
-                Number_Of_People = double.Parse(this.Number_Of_People_TextBox.Text);
-                Tip_Percentage = double.Parse(Tip_Percentage_TextBox.Text);
+
+                Number_Of_People = double.Parse(this.Number_Of_People_TextBox.Text); //Takes imput from TextBox
+
+                Tip_Percentage = double.Parse(Tip_Percentage_TextBox.Text); //Takes imput from TextBox
+
+                /*Below "if" blocks checks if the current value of Tip_Percentage is current more that 100
+                 * , if Number_Of_People is more than 1000 then set it to 1000 and if Number_Of_People is set as 0 then 
+                 * reset 
+                 */
+
                 if (Tip_Percentage > 100)
                 {
                     Tip_Percentage = 100;
                     Tip_Percentage_TextBox.Text = "100";
                 }
+
                 if (Number_Of_People > 1000)
                 {
                     Number_Of_People = 1000;
                     this.Number_Of_People_TextBox.Text = "1000";
                 }
+
                 if (Number_Of_People==0)
                 {
                     Number_Of_People = 1;
                     this.Number_Of_People_TextBox.Text = "1";
                 }
-                if (Bill_Amount < 0 || Number_Of_People < 0 || Tip_Percentage < 0)
-                {
-                    label6.Text = ("Negetive Text Field");
-                }
-                else
-                {
-                    label6.Text = ("");
-                    try
-                    {                    
-                        Total_Per_Person = (Tip_Percentage / 100) * Bill_Amount;
-                        Total_Per_Person = Math.Round((Total_Per_Person / Number_Of_People), 3);
-                    }
-                    catch (System.OverflowException)
-                    {
-                        Total_Per_Person = 0;
-                        labeltax.Text = "$0";
-                    }
 
-                    try
-                    {
-                        Tip_Per_Person = Math.Round((Total_Per_Person + (Bill_Amount / Number_Of_People)), 3);
-                    }
-                    catch (System.OverflowException)
-                    {
-                        Tip_Per_Person = 0;
-                        label8.Text = "$0";
-                    }
-                    labeltax.Text = ("$") + Total_Per_Person.ToString();
-                    label8.Text = ("$") + Tip_Per_Person.ToString();
+                Empty_Field_Alert_Label.Text = ("");//If all the 3 TextBoxes Have valid input then hide Empty_Field_Alert
+
+                /*Below try block calculates Total_Per_Person and check if it exceeds maximum capacity
+                * of "double" data type. If it does then reset it to $0.
+                */
+
+                try
+                {                    
+                    Total_Per_Person = (Tip_Percentage / 100) * Bill_Amount;
+                    Total_Per_Person = Math.Round((Total_Per_Person / Number_Of_People), 3);
                 }
+                catch (System.OverflowException)
+                {
+                    Total_Per_Person = 0;
+                    Tip_Per_Person_Result.Text = "$0";
+                }
+
+                /*Below try block calculates Tip_Per_Person and check if it exceeds maximum capacity
+                * of "double" data type. If it does then reset it to $0.
+                */
+
+                try
+                {
+                    Tip_Per_Person = Math.Round((Total_Per_Person + (Bill_Amount / Number_Of_People)), 3);
+                }
+                catch (System.OverflowException)
+                {
+                    Tip_Per_Person = 0;
+                    Total_Per_Person_Result.Text = "$0";
+                }
+
+                Tip_Per_Person_Result.Text = ("$") + Total_Per_Person.ToString();
+                Total_Per_Person_Result.Text = ("$") + Tip_Per_Person.ToString();
+                
             }
         }
 
-        private void tipminus_Click(object sender, EventArgs e)
-        {
-            if (String.IsNullOrEmpty(Tip_Percentage_TextBox.Text))
-            {
-                Tip_Percentage_TextBox.Text = "0";
-            }
-            else if (double.Parse(Tip_Percentage_TextBox.Text) > 0)
-            {
-                double temp = double.Parse(Tip_Percentage_TextBox.Text) - 1;
-                Tip_Percentage_TextBox.Text = temp.ToString();
-            }
-        }
+        /*Below Button Clicks Method increases and decreases value but first checking 
+         * if TextBox is empty if not, not check for limt 100(for tip%) and 1000(for number of people)
+         * if TextBox is empty set it to 0(for tip%) and 1(for number of people)
+         * */
 
-        private void tipplus_Click(object sender, EventArgs e)
+        private void Tip_Decrease_Button_Click(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(Tip_Percentage_TextBox.Text))
             {
@@ -113,20 +129,20 @@ namespace TipCalculator
             }
         }
 
-        private void pplplus_Click(object sender, EventArgs e)
+        private void Tip_Increase_Button_Click(object sender, EventArgs e)
         {
-            if (String.IsNullOrEmpty(Number_Of_People_TextBox.Text))
+            if (String.IsNullOrEmpty(Tip_Percentage_TextBox.Text))
             {
-                Number_Of_People_TextBox.Text = "1";
+                Tip_Percentage_TextBox.Text = "0";
             }
-            else if (double.Parse(Number_Of_People_TextBox.Text) < 100)
+            else if (double.Parse(Tip_Percentage_TextBox.Text) > 0)
             {
-                double temp = double.Parse(Number_Of_People_TextBox.Text) + 1;
-                Number_Of_People_TextBox.Text = temp.ToString();
+                double temp = double.Parse(Tip_Percentage_TextBox.Text) - 1;
+                Tip_Percentage_TextBox.Text = temp.ToString();
             }
         }
 
-        private void pplminus_Click(object sender, EventArgs e)
+        private void Number_Of_People_Decrease_Button_Click(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(Number_Of_People_TextBox.Text))
             {
@@ -139,32 +155,24 @@ namespace TipCalculator
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Number_Of_People_Increase_Button_Click(object sender, EventArgs e)
         {
-            Calculate_Tip();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            Calculate_Tip();
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-            Calculate_Tip();
-        }
-
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            char ch = e.KeyChar;
-            if (!Char.IsDigit(ch) && ch != 8)
+            if (String.IsNullOrEmpty(Number_Of_People_TextBox.Text))
             {
-                e.Handled = true;
+                Number_Of_People_TextBox.Text = "1";
             }
-
+            else if (double.Parse(Number_Of_People_TextBox.Text) < 100)
+            {
+                double temp = double.Parse(Number_Of_People_TextBox.Text) + 1;
+                Number_Of_People_TextBox.Text = temp.ToString();
+            }
         }
 
-        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        /*
+         * Below KeyPress Method checks if charecter entered is a digit or not.
+         * if not then it wont be added to the TextBox.
+         */
+        private void Bill_Amount_Textbox_KeyPress(object sender, KeyPressEventArgs e)
         {
             char ch = e.KeyChar;
             if (!Char.IsDigit(ch) && ch != 8)
@@ -173,7 +181,36 @@ namespace TipCalculator
             }
         }
 
-        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        /*
+         * Below TextChanged Method only have Digits as input as Filtering is already done by 
+         * KeyPress method, On Every TextChange we calculate Tip.
+         */
+
+        private void Bill_Amount_Textbox_TextChanged(object sender, EventArgs e)
+        {
+            Calculate_Tip();
+        }
+
+        private void Tip_Percentage_TextBox_TextChanged(object sender, EventArgs e)
+        {
+            Calculate_Tip();
+        }
+
+        private void Tip_Percentage_TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Number_Of_People_TextBox_TextChanged(object sender, EventArgs e)
+        {
+            Calculate_Tip();
+        }
+
+        private void Number_Of_People_TextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             char ch = e.KeyChar;
             if (!Char.IsDigit(ch) && ch != 8)
